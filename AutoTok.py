0fda7d0069ece2d6870435a2d7aca25d8444190c
fischerc <fischerc@hu-berlin.de>
2020-09-13 14:52:19 +0200

    add options

--- AutoTok.py
+++ AutoTok.py
@@ -18,12 +18,32 @@
 import glob
 import os
 import subprocess
+import argparse
 
-
-txtfiles =  glob.glob(os.getcwd() + '/*.txt')
-
-for files in txtfiles:
+#define base parameters
+def process_file(files):
     base_name = files.split('.')[0]
     output = base_name + "_neu.xlsx"
+#run .pl script un .txt files
     tokenized = open(output, "w")
     subprocess.call(["perl", "utf8-tokenize.pl", files, output], stdout=tokenized)
+# print final message
+    print(os.path.basename(base_name), "was tokenized successfully")
+
+#end function for optionality of input files
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", nargs="*")
+args = parser.parse_args()
+
+#tokenize list of files in folder
+if args.file:
+    for f in args.file:
+          process_file(f)
+#tokenize all files in folder
+else:
+      txtfiles =  glob.glob(os.getcwd() + '/*.txt')
+
+      for files in txtfiles:
+          process_file(files)
+
+print("\n selected files are tokenized altogeter \n ฅ^•ﻌ•^ฅ")
